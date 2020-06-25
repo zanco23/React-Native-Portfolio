00b2040428579ba9f29ae2b0946e73313393733d
zanco23 <53200172+zanco23@users.noreply.github.com>
2020-06-24 23:59:47 -0700

    Add files via upload

--- Shared/campsites.js
+++ Shared/campsites.js
@@ -1,34 +1,19 @@
-export const CAMPSITES = [
-    {
-        id: 0,
-        name: "Dog Lake Daycare",
-        image: "/assets/images/react-lake.jpg",
-        elevation: 1233,
-        featured: false,
-        description: "Nestled in the foothills of the Chrome Mountains, this animal daycare on the shores of the pristine Lake is a favorite for fly fishers."
-    },
-    {
-        id: 1,
-        name:"Cat River Daycare",
-        image: "/assets/images/chrome-river.jpg",
-        elevation: 877,
-        featured: false,
-        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-    },
-    {
-        id: 2,
-        name:"Sloth Trail Daycare",
-        image: "/assets/images/breadcrumb-trail.jpg",
-        elevation: 2901,
-        featured: false,
-        description: "Let Sloths be your guide to this off-the-beaten-path, hike-in-only campground."
-    },
-    {
-        id: 3,
-        name:"Tiger Woods Daycare",
-        image: "/assets/images/redux-woods.jpg",
-        elevation: 42,
-        featured: true,
-        description: "You'll never want to leave this hidden gem, deep within the lush Tiger Woods."
-    }
-];
+import * as ActionTypes from './ActionTypes';
+
+export const campsites = (state = { isLoading: true,
+                                     errMess: null,
+                                     campsites: []}, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_CAMPSITES:
+            return {...state, isLoading: false, errMess: null, campsites: action.payload};
+
+        case ActionTypes.CAMPSITES_LOADING:
+            return {...state, isLoading: true, errMess: null, campsites: []}
+
+        case ActionTypes.CAMPSITES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
+        default:
+          return state;
+      }
+};
